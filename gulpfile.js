e3958d70b393bec6ab1e0a15002dfe2532fba903
Maxim Pokrovskii <maxim.pokrovskii@gmail.com>
2015-05-23 00:51:25 +0700

    removed mess

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ gulp.task('html', ['styles'], function () {
   return gulp.src('app/*.html')
     .pipe(assets)
     // .pipe($.if('*.js', $.uglify()))
-    .pipe($.if('*.css', $.csso()))
+    // .pipe($.if('*.css', $.csso()))
     .pipe(assets.restore())
     .pipe($.useref())
     // .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
